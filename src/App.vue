2b9dd6e82e01b7b32384644f3d7dc3b01f3174f8
SpreeHertz <spreehertzofficial@gmail.com>
2023-02-28 20:42:06 +0530

    feat: full functionality

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
         </div>
 		
        <div class="delete">
-        <button @click="deletebtn" class="delete-btn" id="delete-btn">Delete</button>
+        <button @click="deleteWebhook" class="delete-btn" id="delete-btn">Delete</button>
        </div>
 	   <div class="success-container hidden" role="alert">
   	<svg aria-hidden="true" class="success-svg" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd"></path></svg>
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
 	name: "App",
 	data() {
@@ -27,23 +29,26 @@ export default {
 		}
 	},
 	methods: {
-		handleInput(event) {
-			const url = event.target.value;
-			console.log(url);
-			if (url.startsWith === 'discord.com/api/webhooks') {
-				return fetch(url, {
-					method: 'DELETE',
-					body: url,
-				}).then(() => {
-					if (response === 200) {
-					console.log('success')
-					}
-				})
-			}
-		
-			
-		}
-	},
+  deleteWebhook() {
+    axios.delete(this.webhookurl)
+      .then(() => {
+        // show success message
+        this.showSuccessMessage();
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  },
+  showSuccessMessage() {
+    // show the success message by removing the "hidden" class from the success container
+    const successContainer = document.querySelector('.success-container');
+    successContainer.classList.remove('hidden');
+  },
+  handleInput(event) {
+    // update the webhookurl data property with the input value
+    this.webhookurl = event.target.value;
+  }
+}
 
 }
 
